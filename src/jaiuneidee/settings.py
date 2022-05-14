865729fc99fd81bddca669ee0bada8fb15fd0783
Rostan Tabet <rostan.tabet@gmail.com>
2022-05-14 23:59:49 +0200

    added a view displaying "Hello, World!"

--- src/jaiuneidee/settings.py
+++ src/jaiuneidee/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'jaiuneidee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ideacrawler/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
