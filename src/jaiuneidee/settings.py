87843185a9ce5d770b64a3ad2eca7da1b2e8536c
Simon Scatton <simon.scatton@epita.fr>
2022-05-15 01:27:39 +0200

    styles: added bootstrap and fontawesome

--- src/jaiuneidee/settings.py
+++ src/jaiuneidee/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'jaiuneidee.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
