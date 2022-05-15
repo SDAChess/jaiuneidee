ba69eebbaf4c29690ddd276d1bcae82404d11b25
Simon Scatton <simon.scatton@epita.fr>
2022-05-15 04:46:06 +0200

    langage: changed language to french

--- src/jaiuneidee/settings.py
+++ src/jaiuneidee/settings.py
@@ -108,13 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+2'
 
 USE_I18N = True
+USE_L10N = True
 
-USE_TZ = True
+LANGUAGE_CODE = 'fr-fr'
 
 
 # Static files (CSS, JavaScript, Images)
