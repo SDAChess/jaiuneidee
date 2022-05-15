22afc01bcdcfb5b7d6afe3db09e1c2b62242b085
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@epita.fr>
2022-05-15 13:13:50 +0200

    ideacrawler: logging in and out now leads to home page instead of Dajngo's default

--- src/jaiuneidee/settings.py
+++ src/jaiuneidee/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
