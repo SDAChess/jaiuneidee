e8770d402442fe7e8fcf21c3635e4d5f85d7db85
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@epita.fr>
2022-05-15 04:36:56 +0200

    ideacrawler: now with login and signup pages !

--- src/ideacrawler/views.py
+++ src/ideacrawler/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.template import loader
+from django.views.decorators.csrf import csrf_protect
 
 
 def index(request):
@@ -15,7 +19,36 @@ def idea(request, idea_id):
     pass
 
 
-def submit(request):
-    template = loader.get_template('ideacrawler/submit.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+@csrf_protect
+def signup(request):
+    #if request.method == "POST":
+    #    form = UserCreationForm(request.POST)
+    #    if form.is_valid():
+    #        form.save()
+    #        username = form.cleaned_data.get('username')
+    #        raw_password = form.cleaned_data.get('password1')
+    #        user = authenticate(username=username, password=raw_password)
+    #        login(request, user)
+
+    #        return redirect('/')
+    #else:
+    #    form = UserCreationForm()
+
+    #return render(request, 'ideacrawler/signup.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print('OUIIIIIIIIII')
+            return redirect('/')
+        else:
+            print('INVALID')
+    else:
+        form = UserCreationForm()
+
+    context = {
+            'form': form
+            }
+
+    return render(request, 'ideacrawler/signup.html', context)
+
