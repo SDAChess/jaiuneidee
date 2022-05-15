a06f090efeceebde69ff00fbde89800f9d05c813
Simon Scatton <simon.scatton@epita.fr>
2022-05-15 04:50:38 +0200

    settings: changed timezone to Europe/Paris

--- src/jaiuneidee/settings.py
+++ src/jaiuneidee/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-TIME_ZONE = 'CEST'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 USE_L10N = True
